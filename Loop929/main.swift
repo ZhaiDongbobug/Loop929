a815ccaa102cbf4062aeb948f85f49f9cce76c3b
ZhaiDongbobug <18747867831@163.com>
2018-09-29 09:02:56 +0800

    1-100累加
    计算从1加到100的代码

---
+++ Loop929/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Loop929
+//
+//  Created by ss20171103190 on 2018/9/29.
+//  Copyright © 2018年 ss20171103190. All rights reserved.
+//
+
+import Foundation
+var i=1
+var Sum=0
+for _ in 1...100
+{
+    Sum+=i
+    i=i+1
+}
+print("%d\n",Sum)
+
